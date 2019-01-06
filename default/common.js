b71883254074793e54afb1394250df416c247c58
Ewelina Kaganiec <kaganiec@gmail.com>
2019-01-06 22:08:47 +0100

    Fix

--- default/common.js
+++ default/common.js
@@ -52,7 +52,7 @@ module.exports = {
 
       if (!target) {
           var towers = creep.room.getTowers();
-          target = creep.pos.findClosestByRange(_.filter(towers, t => t.energy < (tower.energyCapacity/2)));
+          target = creep.pos.findClosestByRange(_.filter(towers, t => t.energy < (t.energyCapacity/2)));
       }
 
       if(!target) {
