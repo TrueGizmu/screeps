f5bed51f63e108a28195de87e09f51b44e80bca3
Ewelina Kaganiec <kaganiec@gmail.com>
2019-01-10 21:46:07 +0100

    Fix for towers

--- default/defences.js
+++ default/defences.js
@@ -10,19 +10,17 @@
 module.exports = {
     run(room) {
         var towers = room.getTowers();
-        
+
         if (!towers) return;
 
         var hostiles = room.find(FIND_HOSTILE_CREEPS);
-        
-        towers.forEach( tower => {
+
+        towers.forEach(tower => {
             var closestHostile = tower.pos.findClosestByRange(hostiles);
             if (closestHostile) {
                 tower.attack(closestHostile);
-                continue;
             }
-            
-            if (tower.energy > 200) {
+            else if (tower.energy > 200) {
                 var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
                     filter: structure => structure.hits < structure.hitsMax && structure.structureType != STRUCTURE_WALL
                 });
